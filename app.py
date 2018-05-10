efd1236568d72ece4f24c42749394fd2c0d39e29
Wykiki <jmegaplay@hotmail.fr>
2018-05-10 14:55:06 +0200

    Add basic rendered page

--- app.py
+++ app.py
@@ -1,12 +1,19 @@
-from flask import Flask
+from flask import Flask, Blueprint
+from flask import render_template
 
 app = Flask(__name__)
 app.config.from_object('config')
 
+bp = Blueprint('profile', __name__,
+               template_folder='template',
+               static_folder='static')
+
+app.register_blueprint(bp)
+
 
 @app.route('/')
 def main():
-    return "not Hello world"
+    return render_template("base.html")
 
 
 if __name__ == '__main__':
